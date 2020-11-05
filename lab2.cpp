87c8ffcaca09c99cf02ef31077ab48957964c8fd
MartaCzapka <01123769@pw.edu.pl>
2020-11-05 11:55:46 +0000

    zad1

--- lab2.cpp
+++ lab2.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+using namespace std;
+class Wektor
+{
+public:
+    double* wektor = new double[n];
+    Wektor(int liczba_el) { n = liczba_el; }
+    ~Wektor() { delete[] wektor; }
 
+private:
+    int n;
+};
 int main()
 {
-    puts("Hello, World!");
+    Wektor wek(2);
+    cout << wek.wektor[1];
 }
