cbb37e3ef793b3ac500040b028b31f5d3c2ba5bd
MartaCzapka <01123769@pw.edu.pl>
2020-11-05 12:03:51 +0000

    zad3

--- lab2.cpp
+++ lab2.cpp
@@ -6,18 +6,24 @@ public:
     double* wektor = new double[n];
     Wektor(int liczba_el)
     {
-        n = liczba_el;
+        dlugosc = liczba_el;
+        n       = liczba_el;
         for (int i = 0; i < n; i++) {
             wektor[i] = 0;
         }
     }
+    int getD() { return dlugosc; }
     ~Wektor() { delete[] wektor; }
 
 private:
     int n;
+    int dlugosc;
 };
 int main()
 {
+    int    dlugosc_wektora;
     Wektor wek(2);
-    cout << wek.wektor[2];
+    cout << wek.wektor[2] << "\n";
+    dlugosc_wektora = wek.getD();
+    cout << dlugosc_wektora;
 }
