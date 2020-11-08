48b6bc129a3b680ead9bd609d48749aff1654feb
MartaCzapka <01123769@pw.edu.pl>
2020-11-08 21:19:07 +0000

    1.12

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -1,8 +1,7 @@
 #pragma once
 
 #include "Resource.hpp"
-#include<iostream>
-using namespace std;
+
 class ResourceManager
 {
   public:
@@ -10,16 +9,12 @@ class ResourceManager
     ResourceManager() { wsk = new Resource;
       wsk=NULL;
       }
-    ResourceManager ( const ResourceManager& re):wsk(re.wsk) {
-   
-      cout<<"konstruktor \n";
-     
-      }
-      ResourceManager operator=( const ResourceManager& re){
-        wsk=re.wsk;
+    ResourceManager ( const ResourceManager& re) : wsk(re->wsk) {}
+      ResourceManager operator=(const ResourceManager& re){
+        wsk=re->wsk;
         return *this;
       }
-      ResourceManager (const ResourceManager&& re):wsk(std::move(wsk.re){}
+      ResourceManager (const ResourceManager&& re) : wsk(std::move(re->wsk){}
     double get() {return wsk->get();}
-    ~ResourceManager() {delete wsk; cout<<"destruktor \n";}
+    ~ResourceManager() {delete wsk;}
 };
