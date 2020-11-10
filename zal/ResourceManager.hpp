db3680e21ad5b616f6cd9dcf4b96cc43069c11c0
MartaCzapka <01123769@pw.edu.pl>
2020-11-10 12:23:48 +0000

    3.15

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -6,7 +6,7 @@ class ResourceManager
  public:
     Resource* wsk;
     ResourceManager ( ){wsk= new Resource; }
-    ResourceManager ( const ResourceManager& re):wsk(re.wsk) {}
+    ResourceManager ( const ResourceManager& re):wsk(re.wsk) {wsk= new Resource{*re.wsk};}
     ResourceManager& operator=(const ResourceManager& re){
       if(wsk == re.wsk) return *this;
       delete wsk;
